ecde1b50a7c303239e532913ed6c7a6c3503a8ea
Harper <Yixuan.18@intl.zju.edu.cn>
2019-03-21 14:57:19 +0800

    Practical5

--- Practical5/counting DNA nucleotides.py
+++ Practical5/counting DNA nucleotides.py
@@ -6,7 +6,6 @@ Created on Wed Mar 20 10:16:22 2019
 """
 
 DNA=input()
-DNA=DNA.split(" ")
 myDict={}
 for word in DNA:
     if word in myDict:
@@ -18,7 +17,7 @@ import matplotlib.pyplot as plt
 labels='A','T','C','G'
 sizes=tuple(myDict.values())
 explode=(0,0.1,0,0)
-fig1,ax1=plt.subplots()
+Fig,ax1=plt.subplots()
 ax1.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=True,startangle=90)
 ax1.axis('equal')
 plt.show()
