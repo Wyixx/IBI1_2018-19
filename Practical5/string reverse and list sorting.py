7e7a42af7fb97640afad5303d38165068c860195
Harper <Yixuan.18@intl.zju.edu.cn>
2019-05-16 22:14:46 +0800

    some pseudocode changed

--- Practical5/string reverse and list sorting.py
+++ Practical5/string reverse and list sorting.py
@@ -7,7 +7,8 @@ Created on Wed Mar 20 11:00:46 2019
 
 s=input('please enter a string of words:\n')
 s=s[ : :-1]
-s=s.split(" ") #get the words from the string, split by blank spaces
+s=s.split(" ") #get the words from the string, split by spaces
+
 #sort & reverse
 L=sorted(s)
 L.reverse()
