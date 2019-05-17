1f2b121e5be732d3d89ad33e0050f08dc2d6729d
Harper <Yixuan.18@intl.zju.edu.cn>
2019-05-17 09:29:40 +0800

    improved 24p

--- Practical7/24points.py
+++ Practical7/24points.py
@@ -6,29 +6,32 @@ Created on Wed Apr  3 09:53:03 2019
 """
 import re
 from fractions import Fraction
+#-------------------verify input numbers--------------------------
 #The input numbers must be interger between 1 and 23
-re_numtest = re.compile(r'(^[1-9]$)|(^1[0-9]$)|(^2[0-3]$)')
+re_numtest = re.compile(r'(^[1-9]$)|(^1[0-9]$)|(^2[0-3]$)')#intergers between 1 and 23
 i = 1
 while i:
     i = 0
     data = input('Please input numbers to computer 24:(use \',\' to divide them)\n')
     numList = data.split(',')
-    for char in numList:
-        if re_numtest.match(char): 
+    for number in numList:
+        if re_numtest.match(number): #test if the input numbers are between 1 and 23
             continue
         else: 
             print('The input number must be intergers from 1 to 23')
-            i = 1
+            i = 1#if the input numbers are not intergers between 1 to 23, input and test again
             break
 
 num = list(map(int,numList))  
-#recursion times
+#---------------------count recursion times------------------------
 count = 0 
 #n is len(num) 
 def dfs(n):
     global count
     count = count +1
 #the mean idea is to reduce the numbers one by one
+#every time, select 2 numbers and perform various operations
+#as 2 numbers run(+=*/) into one, the length of input numbers decreases
     if n == 1:
         if(float(num[0])==24):#1 means the 24 has been calculated successfully
             return 1
@@ -59,7 +62,7 @@ def dfs(n):
             
             if a:
                 #floats are not precise
-                num[i] = Fraction(b,a)
+                num[i] = Fraction(b,a)#handling rational numbers
                 if(dfs(n-1)): 
                     return 1 
                 
@@ -72,6 +75,7 @@ def dfs(n):
             num[j] = b
     return 0 
 
+#-----------------------run the function with inputted numbers------------------
 if (dfs(len(num))): 
     print('Yes')
 else: 
